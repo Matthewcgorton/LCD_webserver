a93c1a1af6139774015f02b0b4a745a156d17e25
Matthew Gorton <matthew@gorton.com>
2020-06-13 15:19:06 -0700

    debug msg

--- app/lcd_hardware.py
+++ app/lcd_hardware.py
@@ -115,13 +115,13 @@ class LCD_Hardware():
 
         def lcd_get_line(self, line):
             line_names = ['line1', 'line2', 'line3', 'line4', ]
-            print(f"line {line}")
+            print(f"\nline {line}")
             if line < 1 or line > 4:
                 return None
 
             line = line - 1
-            print(f"line: {line}, name {line_names[line]}, value {self.lcd_state['msg'].get(line_names[line])}")
-            return self.lcd_state['msg'].get(line_names[line], '')
+            print(f"line: {line}, name {line_names[line]}, value '{self.lcd_state['msg'][line_names[line]])}''")
+            return self.lcd_state['msg'][line_names[line]]
 
         def lcd_get_lines(self):
             lines = []
